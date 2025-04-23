79485dc5dcb96991209aa3912d5b13967e0e5880
Alan <alan.quintanapacheco@gmail.com>
2025-04-23 12:42:36 -0600

    UPDATED: Floating dialog windows on windows rules

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -41,28 +41,28 @@ return {
       vim.cmd("colorscheme sakura")
     end,
   },
-  -- {
-  --   -- Plugin for the Catppuccin color scheme
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   lazy = false, -- Load this plugin immediately
-  --   opts = {
-  --     transparent_background = true, -- Enable transparent background
-  --     flavour = "mocha", -- Set the flavor of the color scheme
-  --   },
-  --   integrations = {
-  --     cmp = true, -- Enable integration with nvim-cmp
-  --     gitsigns = true, -- Enable integration with gitsigns
-  --     nvimtree = true, -- Enable integration with nvim-tree
-  --     treesitter = true, -- Enable integration with treesitter
-  --     notify = false, -- Disable integration with nvim-notify
-  --     mini = {
-  --       enabled = true, -- Enable mini plugin integration
-  --       indentscope_color = "", -- Set indentscope color (empty means default)
-  --     },
-  --     -- Additional plugin integrations can be found in the documentation
-  --   },
-  -- },
+  {
+    -- Plugin for the Catppuccin color scheme
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false, -- Load this plugin immediately
+    opts = {
+      transparent_background = true, -- Enable transparent background
+      flavour = "mocha", -- Set the flavor of the color scheme
+    },
+    integrations = {
+      cmp = true, -- Enable integration with nvim-cmp
+      gitsigns = true, -- Enable integration with gitsigns
+      nvimtree = true, -- Enable integration with nvim-tree
+      treesitter = true, -- Enable integration with treesitter
+      notify = false, -- Disable integration with nvim-notify
+      mini = {
+        enabled = true, -- Enable mini plugin integration
+        indentscope_color = "", -- Set indentscope color (empty means default)
+      },
+      -- Additional plugin integrations can be found in the documentation
+    },
+  },
   -- {
   --   -- Plugin for the Modus Themes
   --   "miikanissi/modus-themes.nvim",
@@ -184,8 +184,8 @@ return {
     -- LazyVim configuration
     "LazyVim/LazyVim",
     opts = {
-      -- Set the default color scheme
-      colorscheme = "oldworld",
+      --color Set the default color scheme
+      colorscheme = "default",
     },
   },
 }
